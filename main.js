587de9ef2e1a7ad4f97c64999c3c0481923457e5
GaneshSAcharya <31121986g@gmail.com>
2018-06-23 07:57:43 +0530

    function call in ES6

--- main.js
+++ main.js
@@ -1,16 +1,26 @@
-function resolveAfter2Seconds() {
-    return new Promise(resolve => {
-      setTimeout(() => {
-        resolve('resolved');
-      }, 2000);
-    });
-  }
+// function resolveAfter2Seconds() {
+//     return new Promise(resolve => {
+//       setTimeout(() => {
+//         resolve('resolved');
+//       }, 2000);
+//     });
+//   }
   
-  async function asyncCall() {
-    console.log('calling');
-    var result = await resolveAfter2Seconds();
-    console.log(result);
-    // expected output: "resolved"
-  }
+//   async function asyncCall() {
+//     console.log('calling');
+//     var result = await resolveAfter2Seconds();
+//     console.log(result);
+//     // expected output: "resolved"
+//   }
   
-  asyncCall();
+//   asyncCall();
+
+
+
+var test = () => {
+
+    console.log('ES6!')
+
+};
+
+test();
